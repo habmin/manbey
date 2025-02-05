d7b8b492494a10a3d5caa6b3c47ab3237d28aadf
habmin <henryandrewbaum@gmail.com>
2025-02-05 01:26:06 -0500

    fixed removeActiveZone

--- vida.fix.js
+++ vida.fix.js
@@ -1,3 +1,5 @@
-p5.prototype.Vida.prototype.removeActiveZone = function(t) {
-    for (var s = this.activeZones.length - 1; s >= 0; s--) t == this.activeZones[s].id && this.activeZones.splice(s, 1)
+p5.prototype.Vida.prototype.removeActiveZone = function(_id) {
+    for(var i = this.activeZones.length - 1; i >= 0; i--) {
+      if(_id == this.activeZones[i].id) this.activeZones.splice(i, 1);
+    }
   }
